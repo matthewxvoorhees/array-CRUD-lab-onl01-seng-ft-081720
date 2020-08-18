28aaf69db8f9937b424ac3b079f85ab5c8d45150
Matthew Crnich <matthew.crnich@gmail.com>
2020-08-18 21:12:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,8 +2,8 @@ def create_an_empty_array
   []
 end
 
-def create_an_array = 
-  "Matt", "Flat", "Hat", "Rat"]
+def create_an_array
+an_array = ["Matt", "Flat", "Hat", "Rat"]
 end
 
 def add_element_to_end_of_array(array, element)
